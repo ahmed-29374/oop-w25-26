b331697c327810a02b6a9f0515d249f3787ee655
ahmed-29374 <ia7mdxxx@gmail.com>
2025-12-03 14:47:16 +0000

    Add main file

--- students/35688-AhmedMohammed/python-app-2/main.py
+++ students/35688-AhmedMohammed/python-app-2/main.py
@@ -1,1 +1,20 @@
+#This is a comment.
+print("Ahmed Mohamed")
 
+# 3 variables with numbers (integers)
+a = 5
+b = 12
+c = 30
+
+# 3 variables with text (strings)
+name = "Ahmed"
+greeting = "hello"
+message = 'how are you'
+
+# display all the variables
+print(a)
+print(b)
+print(c)
+print(name)
+print(greeting)
+print(message)
