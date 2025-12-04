c76f582945526b843469da61171353c55903b9b7
ahmed-29374 <ia7mdxxx@gmail.com>
2025-12-04 11:25:15 +0000

    Add main file

--- students/35688-AhmedMohammed/main.py
+++ students/35688-AhmedMohammed/main.py
@@ -1,1 +1,20 @@
+#This is a comment that does not affect how the code works.
 
+#Loop
+for i in range(10):
+    print("Ahmed")
+
+#Functions
+def my_name():
+  print("Your name is ahmed")
+
+  #Lists
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False]
+
+#Dictionaries
+"Dictionaries are used to store data values in key:value pairs."
+person={"name":"ahmed" ,
+         "age":20 ,
+         "city":"warsaw"}
