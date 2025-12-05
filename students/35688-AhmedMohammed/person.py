2c05e4678acabd5ad8da73fa7d8a0ca67c056cdd
ahmed-29374 <ia7mdxxx@gmail.com>
2025-12-05 16:02:58 +0000

    Add main file

--- students/35688-AhmedMohammed/person.py
+++ students/35688-AhmedMohammed/person.py
@@ -1,1 +1,11 @@
+class Person:
+    def __init__(self, id, name, last_name, age, created_at, updated_at):
+        self.id=id
+        self.name=name
+        self.last_name=last_name
+        self.age=age
+        self.created_at=created_at
+        self.updated_at=updated_at
 
+person_1=
+        
